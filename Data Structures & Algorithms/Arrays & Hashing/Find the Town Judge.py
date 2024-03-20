b7860469a721e7b860cc97d3f5ab7e36b66f4d35
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-03-19 20:27:37 -0400

    Create Find the Town Judge.py

---
+++ Data Structures & Algorithms/Arrays & Hashing/Find the Town Judge.py
@@ -0,0 +1,15 @@
+import defaultdict from collections
+class Solution(object):
+    def findJudge(self, n, trust):
+        trusts = defaultdict(int)
+        trusted = defaultdict(int)
+        
+        for a, b in trust:
+            trusts[a] += 1
+            trusted[b] += 1
+        
+        for i in range(1, n + 1):
+            if trusts[i] == 0 and trusted[i] == n - 1:
+                return i
+        
+        return -1
