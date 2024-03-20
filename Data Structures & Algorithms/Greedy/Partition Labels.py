4ab10765dd70e71e2c6db9b549e7fb9a624bc4ce
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-03-19 22:07:38 -0400

    Update Partition Labels.py

--- Data Structures & Algorithms/Greedy/Partition Labels.py
+++ Data Structures & Algorithms/Greedy/Partition Labels.py
@@ -1,24 +1,18 @@
 class Solution(object):
     def partitionLabels(self, s):
-        freq = {}
-        result = []
-        charSet = set()
-
-        for c in s:
-            freq[c] = 1 + freq.get(c, 0)
-        
-        p = 0
+        hash = {}
+        res = []
 
         for i in range(len(s)):
-            if len(charSet) == 0 and i - p > 0:
-                result.append(i - p)
-                p = i
+            hash[s[i]] = i # replaces after every occurrence 
+
+        curLen, last = 0, 0
+        for i in range(len(s)):
+            last = max(last, hash[s[i]])
+            curLen += 1
             
-            charSet.add(s[i])   
-            freq[s[i]] -= 1
-            
-            if freq[s[i]] == 0:
-                charSet.remove(s[i])
-        
-        result.append(len(s) - p)
-        return result
+            if i == last:
+                res.append(i)
+                curLen = 0
+
+        return res
