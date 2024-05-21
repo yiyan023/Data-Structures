a551c619564fa6071071c545c0bf751f93016aca
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-05-21 10:09:23 -0400

    Create Matchsticks to Squares.py

---
+++ Data Structures & Algorithms/Backtracking/Matchsticks to Squares.py
@@ -0,0 +1,26 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        length = sum(matchsticks) // 4
+        sides = [0] * 4
+
+        if length != sum(matchsticks) / 4:
+            return False 
+        
+        matchsticks.sort(reverse = True)
+        
+        def dfs(i):
+            if i == len(matchsticks):
+                return True # made it to the end 
+            
+            for j in range(4):
+                if sides[j] + matchsticks[i] <= length:
+                    sides[j] += matchsticks[i]
+
+                    if dfs(i + 1):
+                        return True
+                    
+                    sides[j] -= matchsticks[i] 
+            
+            return False
+        
+        return dfs(0)
