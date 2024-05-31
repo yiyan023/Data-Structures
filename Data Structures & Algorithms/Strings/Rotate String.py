e57bbcbb588911080bfe348345d63801970f4972
Yiyan Huang <56096857+yiyan023@users.noreply.github.com>
2024-05-31 15:09:48 -0400

    Create Rotate String.py

---
+++ Data Structures & Algorithms/Strings/Rotate String.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False 
+    
+        for i in range(len(goal)):
+            if goal[i:] + goal[:i] == s:
+                return True 
+        
+        return False 
