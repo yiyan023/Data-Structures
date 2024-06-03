d3c4feedc93302340c7ffdbc023de878341a59bd
Yiyan Huang <56096857+yiyan023@users.noreply.github.com>
2024-06-03 15:48:34 -0400

    Create Number of Dice Rolls with Target Sum.py

---
+++ Data Structures & Algorithms/Dynamic Programming/Number of Dice Rolls with Target Sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        MOD = 10 ** 9 + 7
+
+        for dice in range(n):
+            next_dp = [0] * (target + 1)
+
+            for val in range(1, k + 1):
+                for total in range(val, target + 1):
+                    next_dp[total] = (next_dp[total] + dp[total - val]) % MOD 
+            
+            dp = next_dp
+            print(dp)
+        
+        return dp[target]
