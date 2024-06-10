6b3df7c896e1c98cba43dfd2427b3801418749f8
Yiyan Huang <56096857+yiyan023@users.noreply.github.com>
2024-06-10 10:58:24 -0400

    Create Height Checker.py

---
+++ Data Structures & Algorithms/Arrays & Hashing/Height Checker.py
@@ -0,0 +1,11 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        
+        sorted_heights = sorted(heights)
+        unexpected = 0
+
+        for i in range(len(heights)):
+            if heights[i] != sorted_heights[i]:
+                unexpected += 1
+
+        return unexpected
