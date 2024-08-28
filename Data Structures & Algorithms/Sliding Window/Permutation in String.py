607101a5e946bde309beecb46c8b008e06d3e169
Yiyan Huang <56096857+yiyan023@users.noreply.github.com>
2024-08-28 16:25:07 -0400

    Update Permutation in String.py

--- Data Structures & Algorithms/Sliding Window/Permutation in String.py
+++ Data Structures & Algorithms/Sliding Window/Permutation in String.py
@@ -1,32 +1,25 @@
 class Solution:
-    def checkInclusion(self, s1, s2):
-        count1, count2 = {}, {}
-        l = 0
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        freq1, freq2 = defaultdict(int), defaultdict(int)
 
         for c in s1:
-            count1[c] = 1 + count1.get(c, 0)
+            freq1[c] = 1 + freq1.get(c, 0)
         
+        l = 0
+
         for r in range(len(s2)):
-            if count1 == count2:
-                return True 
-            
-            if s2[r] in count1:
-                count2[s2[r]] = 1 + count2.get(s2[r], 0)
+            freq2[s2[r]] = 1 + freq2.get(s2[r], 0)
 
-            while l < r:
-                if s2[r] in count1 and s2[l] in count1 and count2[s2[r]] <= count1[s2[r]]:
-                    break
+            while freq2[s2[r]] > freq1[s2[r]]:
+                if s2[l] in freq2:
+                    freq2[s2[l]] -= 1
+
+                    if freq2[s2[l]] == 0:
+                        freq2.pop(s2[l])
                 
-                if s2[r] not in count1:
-                    if s2[l] in count1: 
-                        count2[s2[l]] -= 1
-
-                elif count2[s2[r]] > count1[s2[r]] and s2[l] in count1: 
-                    count2[s2[l]] -= 1
-
-                if s2[l] in count2 and count2[s2[l]] == 0:
-                    count2.pop(s2[l])
-                        
                 l += 1
 
-        return count1 == count2
+            if freq1 == freq2:
+                return True 
+        
+        return False
