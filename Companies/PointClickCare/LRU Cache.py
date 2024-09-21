f2b0fef94822d34698b9645b8b9f8c24a00ffeae
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-09-21 18:44:48 -0400

    Create LRU Cache.py

---
+++ Companies/PointClickCare/LRU Cache.py
@@ -0,0 +1,25 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.cache[key] = self.cache.pop(key)
+            return self.cache[key]
+        
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        self.cache.pop(key, None)
+        self.cache[key] = value 
+
+        if len(self.cache) > self.capacity:
+            self.cache.pop(next(iter(self.cache)))
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
