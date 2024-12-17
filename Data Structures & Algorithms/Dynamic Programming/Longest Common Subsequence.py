ac8d219aba89d3761157dc9e799d208c570945e9
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-12-17 16:23:09 -0500

    Update Longest Common Subsequence.py

--- Data Structures & Algorithms/Dynamic Programming/Longest Common Subsequence.py
+++ Data Structures & Algorithms/Dynamic Programming/Longest Common Subsequence.py
@@ -1,12 +1,19 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+        # check for equality 
+        if text1 == text2:
+            return len(text1)
+        
+        dp = [[0 for _ in range(len(text1))] for _ in range(len(text2))]
 
-        for i in range(1, len(text1) + 1):
-            for j in range(1, len(text2) + 1):
-                if text1[i-1] == text2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+        for i in range(len(text2)):
+            for j in range(len(text1)):
+                if text2[i] == text1[j]:
+                    dp[i][j] = 1 if i-1 < 0 or j - 1 < 0 else dp[i-1][j-1] + 1
+                
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
-        return dp[len(text1)][len(text2)]
+                    prev_turn = dp[i-1][j] if i - 1 >= 0 else 0
+                    prev_val = dp[i][j-1] if j - 1 >= 0 else 0
+                    dp[i][j] = max(prev_turn, prev_val)
+        print(dp)
+        return dp[-1][-1]
