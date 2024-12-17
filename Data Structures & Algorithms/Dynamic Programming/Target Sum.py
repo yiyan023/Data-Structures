a29b7eaf5ba0cbcc0565de0d0b6c1f18e0a2841c
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-12-17 18:21:40 -0500

    Update Target Sum.py

--- Data Structures & Algorithms/Dynamic Programming/Target Sum.py
+++ Data Structures & Algorithms/Dynamic Programming/Target Sum.py
@@ -1,18 +1,17 @@
 class Solution:
-  def findTargetSubsets(self, num, s):
-    # TODO: Write your code here
-    adjusted_sum = sum(num) + s
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        new_sum = sum(nums) + target
 
-    if adjusted_sum % 2 != 0:
-      return 0
-    
-    target_sum = adjusted_sum // 2
-    dp = [0 for _ in range(target_sum + 1)]
-    dp[0] = 1
+        if new_sum % 2 != 0 or abs(sum(nums)) < abs(target):
+            return 0
+        
+        target_sum = new_sum // 2
 
-    for n in num:
-      for curr_sum in range(target_sum, n - 1, -1):
-        dp[curr_sum] += dp[curr_sum - n]
-        # print(dp)
-    
-    return dp[-1]
+        dp = [0 for _ in range(target_sum + 1)] 
+        dp[0] = 1
+
+        for num in nums:
+            for curr_sum in range(target_sum, num - 1, -1):
+                dp[curr_sum] += dp[curr_sum - num]
+        
+        return dp[-1]
