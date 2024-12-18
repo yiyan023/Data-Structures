0e90e6e5b088ebe857b969e5e405bfb65edf93fe
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-12-18 16:44:22 -0500

    Update Island Perimeter.py

--- Data Structures & Algorithms/Graphs/BFS/Island Perimeter.py
+++ Data Structures & Algorithms/Graphs/BFS/Island Perimeter.py
@@ -1,39 +1,25 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        # perimeter of one unit is equal to 4 - # of neighbours 
-        queue = collections.deque()
-        visited = set()
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
-        def in_grid(r, c):
-            return 0 <= r < len(grid) and 0 <= c < len(grid[0])
-
-        def bfs(r, c):
-            visited.add((r, c))
-            perimeter = 0
-
-            while queue:
-                x, y = queue.popleft()
-                edges = 0
-
-                for dx, dy in directions:
-                    new_x, new_y = x + dx, y + dy
-
-                    if in_grid(new_x, new_y) and grid[new_x][new_y] == 1:
-                        edges += 1
-
-                        if (new_x, new_y) not in visited:
-                            visited.add((new_x, new_y))
-                            queue.append((new_x, new_y))
-                
-                perimeter += (4 - edges)
-
-            return perimeter
-
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == 1:
-                    queue.append((r, c))
-                    return bfs(r, c)
-        
-        return 0
+    
+        rows = len(grid)
+        cols = len(grid[0])
+        perimeter = 0
+    
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:  # If the cell is land
+                # Check all four directions
+                # Up
+                    if i == 0 or grid[i - 1][j] == 0:
+                        perimeter += 1
+                # Down
+                    if i == rows - 1 or grid[i + 1][j] == 0:
+                        perimeter += 1
+                # Left
+                    if j == 0 or grid[i][j - 1] == 0:
+                        perimeter += 1
+                # Right
+                    if j == cols - 1 or grid[i][j + 1] == 0:
+                        perimeter += 1
+    
+        return perimeter
