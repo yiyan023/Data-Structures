d3b9753dae817087679e32f64a523c7fa727bb8e
Yiyan Huang <yiyanhuang0523@gmail.com>
2025-07-09 11:36:49 -0700

    Update Minimum Increments to Equalize Leaf Paths.py

--- Data Structures & Algorithms/Categories/BST/Minimum Increments to Equalize Leaf Paths.py
+++ Data Structures & Algorithms/Categories/BST/Minimum Increments to Equalize Leaf Paths.py
@@ -1,54 +1,37 @@
 class Solution:
     def minIncrease(self, n: int, edges: List[List[int]], cost: List[int]) -> int:
         neighbours = defaultdict(list)
-        max_paths = [0] * len(cost)
         seen = set()
+        res = 0
 
         for s, e in edges:
             neighbours[s].append(e)
             neighbours[e].append(s)
         
+        test = []
+        
         def dfs(node, acc):
+            nonlocal res
             if node and len(neighbours[node]) == 1:
-                max_paths[node] = max(max_paths[node], cost[node])
                 return cost[node]
             
             seen.add(node)
+            child_paths = []
 
             for child in neighbours[node]:
                 if child not in seen:
                     seen.add(child)
 
                     child_path = dfs(child, acc + cost[node])
-                    max_paths[node] = max(max_paths[node], cost[node] + child_path)
+                    child_paths.append(child_path)
             
-            return max_paths[node]
+            max_path = max(child_paths)
+
+            for child_path in child_paths:
+                if child_path != max_path:
+                    res += 1
+
+            return max_path + cost[node]
         
         dfs(0, 0)
-        seen = set()
-        res = 0
-        
-        def count_changes(node, acc):
-            nonlocal res
-            if acc + max_paths[node] != max_paths[0]:
-                acc += (max_paths[0] - acc - max_paths[node])
-                res += 1
-            
-            seen.add(node)
-            
-            for child in neighbours[node]:
-                if child not in seen:
-                    seen.add(child)
-                    count_changes(child, acc + cost[node])
-
-            return 
-        
-        count_changes(0, 0)
         return res
-
-                
-        
-
-
-
-            
